52dda308193f1be9533cd5d25e8337d42df81d85
EMON <95125907+Mdemon99@users.noreply.github.com>
2022-01-20 21:43:57 +0600

    Create SSB1.py

---
+++ SSB1.py
@@ -0,0 +1,18 @@
+import os,sys,random,struct
+
+x = str(struct.calcsize("P") * 8)
+
+if '32' in x:
+
+    os.system('chmod 777 s32 && ./s32')
+
+elif '32' in x:
+
+    os.system('chmod 777 s32 && ./s32')
+
+else:
+
+    print('\033[1;31m   aarch cannot identified\033[0;97m')
+    os.system('xdg-open https://Facebook/MDEMONISLAM99')
+    os.sys.exit()
+
